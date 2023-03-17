826a8a6b74bc1be4c8b54b6a8b0ed38ed931872b
joshhoneypot <joshuaangugo@gmail.com>
2023-03-17 10:01:19 +0300

    arithmetic labs

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod); 
+
+let set = [4, 12, 5, 20, 2, 12];
+let max = Math.max(...set);
+console.log(max);
